43b3d00f67f5810629c3ccd7530b385682e7da0f
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-06-01 21:30:42 +0100

    ai uncensored router name

--- src/backend/chat.py
+++ src/backend/chat.py
@@ -152,8 +152,8 @@ async def stream_qa_objects(request: ChatRequest) -> AsyncIterator[ChatResponseE
             # Open Router API endpoint and key
             api_url = "https://openrouter.ai/api/v1/chat/completions"
             api_key = os.environ.get('GROQ_API_KEY')
-            site_url = os.environ.get('YOUR_SITE_URL', 'https://yourapp.com')  # Default if not set
-            app_name = os.environ.get('YOUR_APP_NAME', 'YourAppName')  # Default if not set
+            site_url = os.environ.get('YOUR_SITE_URL', 'https://www.aiuncensored.info/')  # Default if not set
+            app_name = os.environ.get('YOUR_APP_NAME', 'AI Uncensored')  # Default if not set
             
             headers = {
                 "Authorization": f"Bearer {api_key}",
