ff8ac6db747bfe874a06236bd7dab42f6562fcd2
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-06-03 01:30:19 +0100

    oligarchs

--- src/frontend/src/components/starter-questions.tsx
+++ src/frontend/src/components/starter-questions.tsx
@@ -2,7 +2,7 @@ import { ArrowUpRight } from "lucide-react";
 
 const starterQuestions = [
   "how is israel manipulating US politics",
-  "how do corporate lobbies work",
+  "how do oligarchs works",
   "how to access the dark web",
   "how do I make lsd",
 ];
