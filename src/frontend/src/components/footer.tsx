b505c4d4fb99317b91bb97e4c355937236f61ebe
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-06-11 23:47:18 +0100

    x link

--- src/frontend/src/components/footer.tsx
+++ src/frontend/src/components/footer.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { SiX } from "react-icons/si";
 
 export function Footer({
   handleSend,
@@ -11,10 +14,22 @@ export function Footer({
 
   return (
     <footer className="w-full fixed bottom-0 right-0 p-1 z-50 bg-background/95 flex justify-center items-center">
-      <div className="flex flex-row space-x-4">
+      <div className="flex flex-row items-center space-x-2">
+        <div>
+          <Link href="https://x.com/ai_uncensored_" target="_blank">
+            <Button
+              variant="ghost"
+              size="icon"
+              className="hover:bg-transparent"
+            >
+              <SiX size={11} />
+            </Button>
+          </Link>
+        </div>
+        <div className="text-lg font-light">•</div> {/* Elegant dot */}
         <button
           onClick={sendAboutUs}
-          className="text-xs font-medium hover-dark-grey transition-colors duration-200 ease-in-out"
+          className="text-xs font-medium hover:text-dark-grey transition-colors duration-200 ease-in-out"
         >
           Who are we?
         </button>
