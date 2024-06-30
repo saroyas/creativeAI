1a399ea50b82f61f279e21d697fc550420b12578
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-06-30 04:41:38 +0100

    no x

--- src/frontend/src/components/footer.tsx
+++ src/frontend/src/components/footer.tsx
@@ -15,18 +15,6 @@ export function Footer({
   return (
     <footer className="w-full fixed bottom-0 right-0 p-1 z-50 bg-background/95 flex justify-center items-center">
       <div className="flex flex-row items-center space-x-2">
-        <div>
-          <Link href="https://x.com/ai_uncensored_" target="_blank">
-            <Button
-              variant="ghost"
-              size="icon"
-              className="hover:bg-transparent"
-            >
-              <SiX size={11} />
-            </Button>
-          </Link>
-        </div>
-        <div className="text-lg font-light">•</div> {/* Elegant dot */}
         <button
           onClick={sendAboutUs}
           className="text-xs font-medium hover:text-dark-grey transition-colors duration-200 ease-in-out"
