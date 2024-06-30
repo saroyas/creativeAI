c4874d972c47b23806f60e327a28da8d25e78982
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-06-30 18:59:54 +0100

    typo

--- src/frontend/src/components/nav.tsx
+++ src/frontend/src/components/nav.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 import { useTheme } from "next-themes";
 
+export function Navbar() {
   const { theme } = useTheme();
   return (
     <header className="w-full flex fixed p-1 z-50 px-2 bg-background justify-between items-center">
