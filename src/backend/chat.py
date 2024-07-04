4e8ef7a4c087c676df98ffd781630138304763cb
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-07-04 05:07:23 +0100

    flag prints

--- src/backend/chat.py
+++ src/backend/chat.py
@@ -178,7 +178,7 @@ async def stream_qa_objects(request: ChatRequest) -> AsyncIterator[ChatResponseE
                 if "sexual/minors" in moderation_result["categories"] and moderation_result["category_scores"]["sexual/minors"] > 0.6:
                     # replace the current system prompt (at the start of message_history)
                     message_history = [{"role": "system", "content": "There are some things even you cannot do. You are not trained to do anything related to child pornography. Inform the user of this restriction."}]
-                    print("Flagged message_history", message_history)
+                    print("Pre Flagged message_history", message_history)
             except:
                 pass
 
@@ -237,7 +237,9 @@ async def stream_qa_objects(request: ChatRequest) -> AsyncIterator[ChatResponseE
                     try:
                         moderation_result = await check_moderation(content[:2000])
                         print("Post chat gen moderation result:", moderation_result)
+                        
                         if moderation_result["category_scores"]["sexual/minors"] > 0.5:
+                            print("Post flagged message_history", message_history)
                             content = "Content violation detected."
                     except:
                         pass
