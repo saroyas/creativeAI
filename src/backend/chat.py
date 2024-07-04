5adae062aca7002a7fc8b04108d582589399672e
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-07-04 03:22:39 +0100

    revert

--- src/backend/chat.py
+++ src/backend/chat.py
@@ -30,8 +30,11 @@ from backend.utils import is_local_model
 # Define the "About Us" message
 WHO_ARE_WE_MESSAGE = """
 We are hackers and artists.
-We believe in access to unrestricted information.
+We believe in access to free information.
 No matter who you are. No matter where.
+
+A lot of our traffic is from Iran, Russia and China.
+Where censorship is rife, and dissent silenced.
 
 We do not store anything. We ask no details.
 We recommend you generally use a VPN.
