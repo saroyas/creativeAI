cddb0a960ff38747395e48c9ac60921f8f11a057
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-07-05 19:57:46 +0100

    next public

--- src/frontend/src/components/image-panel.tsx
+++ src/frontend/src/components/image-panel.tsx
@@ -14,7 +14,7 @@ import { FiLink, FiTwitter, FiFacebook, FiDownload } from 'react-icons/fi';
 import { FaRedditAlien, FaWhatsapp } from 'react-icons/fa';
 
 const BASE_URL = env.NEXT_PUBLIC_API_URL;
-const FREEIMAGE_HOST_API_KEY = env.FREEIMAGE_HOST_API_KEY;
+const FREEIMAGE_HOST_API_KEY = env.NEXT_PUBLIC_FREEIMAGE_HOST_API_KEY;
 
 type ImageModel = 'anime' | 'photo';
 type ImageAspect = 'square' | 'landscape' | 'portrait';
--- src/frontend/src/env.mjs
+++ src/frontend/src/env.mjs
@@ -8,11 +8,11 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_API_URL: z.string(),
     NEXT_PUBLIC_LOCAL_MODE_ENABLED: z.boolean().default(false),
-    FREEIMAGE_HOST_API_KEY: z.string(),
+    NEXT_PUBLIC_FREEIMAGE_HOST_API_KEY: z.string(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    FREEIMAGE_HOST_API_KEY: process.env.FREEIMAGE_HOST_API_KEY,
+    NEXT_PUBLIC_FREEIMAGE_HOST_API_KEY: process.env.NEXT_PUBLIC_FREEIMAGE_HOST_API_KEY,
     NEXT_PUBLIC_LOCAL_MODE_ENABLED:
       process.env.NEXT_PUBLIC_LOCAL_MODE_ENABLED === "true",
   },
