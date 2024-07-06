a9ab70ab6509dafde7a488cd22334bb66781796c
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-07-06 19:40:23 +0100

    rev small cross button

--- src/frontend/src/app/image/[imageCode]/page.tsx
+++ src/frontend/src/app/image/[imageCode]/page.tsx
@@ -2,7 +2,6 @@
 import { ImagePanel } from "@/components/image-panel";
 import { useEffect } from 'react';
 import { event } from 'nextjs-google-analytics';
-import Link from "next/link";
 
 interface PageProps {
   params: {
@@ -14,6 +13,7 @@ export default function Home({ params }: PageProps) {
   const { imageCode } = params;
 
   useEffect(() => {
+    // Send Google Analytics event when the page loads
     event('Share_Image_Opened', {
       category: 'Share_Image_Opened',
       label: imageCode ? 'Share_Image_Opened Chat' : 'New Image',
@@ -22,14 +22,8 @@ export default function Home({ params }: PageProps) {
   }, [imageCode]);
 
   return (
-    <div className="min-h-[100dvh] flex flex-col relative bg-black text-white">
-      <Link 
-        href="/" 
-        className="absolute top-4 left-4 w-8 h-8 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition-colors duration-300 z-10"
-      >
-        <span className="text-2xl leading-none">&times;</span>
-      </Link>
-      <main className="flex-grow flex items-center justify-center">
+    <div className="h-[100dvh] flex flex-col">
+      <main className="flex-grow flex items-stretch justify-center">
         <ImagePanel initialImageCode={imageCode} />
       </main>
     </div>
--- src/frontend/src/app/image/page.tsx
+++ src/frontend/src/app/image/page.tsx
@@ -1,16 +1,9 @@
 import { ImagePanel } from "@/components/image-panel";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-[100dvh] flex flex-col relative bg-black text-white">
-      <Link 
-        href="/" 
-        className="absolute top-4 left-4 w-8 h-8 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition-colors duration-300 z-10"
-      >
-        <span className="text-2xl leading-none">&times;</span>
-      </Link>
-      <main className="flex-grow flex items-center justify-center">
+    <div className="h-[100dvh] flex flex-col">
+      <main className="flex-grow flex items-stretch justify-center">
         <ImagePanel />
       </main>
     </div>
