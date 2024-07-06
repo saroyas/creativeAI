ceb3a97d25e83804673de30d06e2637c430655b9
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-07-06 19:22:19 +0100

    cross

--- src/frontend/src/app/image/[imageCode]/page.tsx
+++ src/frontend/src/app/image/[imageCode]/page.tsx
@@ -2,6 +2,7 @@
 import { ImagePanel } from "@/components/image-panel";
 import { useEffect } from 'react';
 import { event } from 'nextjs-google-analytics';
+import Link from "next/link";
 
 interface PageProps {
   params: {
@@ -22,8 +23,14 @@ export default function Home({ params }: PageProps) {
   }, [imageCode]);
 
   return (
-    <div className="h-[100dvh] flex flex-col">
-      <main className="flex-grow flex items-stretch justify-center">
+    <div className="h-[100dvh] flex flex-col relative bg-black text-white">
+      <Link 
+        href="/" 
+        className="absolute top-4 left-4 w-8 h-8 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition-colors duration-300 z-10"
+      >
+        <span className="text-2xl leading-none">&times;</span>
+      </Link>
+      <main className="flex-grow flex items-stretch justify-center p-4">
         <ImagePanel initialImageCode={imageCode} />
       </main>
     </div>
