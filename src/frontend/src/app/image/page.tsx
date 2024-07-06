5e1bf789e58730413d4e9b15d9b27c82823f97f6
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-07-06 19:37:42 +0100

    update

--- src/frontend/src/app/image/page.tsx
+++ src/frontend/src/app/image/page.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-[100dvh] flex flex-col relative bg-black text-white">
+    <div className="min-h-[100dvh] flex flex-col relative bg-black text-white">
       <Link 
         href="/" 
-        className="absolute top-4 left-4 w-8 h-8 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition-colors duration-300"
+        className="absolute top-4 left-4 w-8 h-8 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition-colors duration-300 z-10"
       >
         <span className="text-2xl leading-none">&times;</span>
       </Link>
-      <main className="flex-grow flex items-stretch justify-center p-4">
+      <main className="flex-grow flex items-center justify-center">
         <ImagePanel />
       </main>
     </div>
