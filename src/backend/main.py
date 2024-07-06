678de45825623da8ae2b2623357515f9b64ddc02
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-07-06 23:12:52 +0100

    minor adjustments

--- src/backend/main.py
+++ src/backend/main.py
@@ -291,7 +291,7 @@ async def generate_image_async(task_id: str, prompt: str, imageURL: str, model: 
     }
     if imageURL and imageURL != "":
         print("Using image URL", imageURL)
-        payload["prompt"] = ""
+        # payload["prompt"] = ""
         payload["imageUrl"] = imageURL
         payload["denoising_strength"] = 0.8
 
