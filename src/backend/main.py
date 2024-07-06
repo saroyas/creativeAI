7552402421137381e56bdd9278792282b46099e4
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-07-06 14:24:57 +0100

    0.9

--- src/backend/main.py
+++ src/backend/main.py
@@ -292,7 +292,7 @@ async def generate_image_async(task_id: str, prompt: str, imageURL: str, model: 
     if imageURL and imageURL != "":
         print("Using image URL", imageURL)
         payload["imageUrl"] = imageURL
-        payload["denoising_strength"] = 0.85
+        payload["denoising_strength"] = 0.9
 
     async with httpx.AsyncClient() as client:
         try:
