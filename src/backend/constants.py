715fb8760f1d8b8cf1b0ccca2ecc8c0b7560731b
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-07-23 18:37:05 +0100

    model mappings

--- src/backend/constants.py
+++ src/backend/constants.py
@@ -4,6 +4,7 @@ GPT4_MODEL = "gpt-4o"
 GPT3_MODEL = "gpt-3.5-turbo"
 LLAMA_8B_MODEL = "llama3-8b-8192"
 LLAMA_70B_MODEL = "llama3-70b-8192"
+LLAMA_3_405B = "llama3-405b"
 
 LOCAL_LLAMA3_MODEL = "llama3"
 LOCAL_GEMMA_MODEL = "gemma:7b"
@@ -11,7 +12,7 @@ LOCAL_MISTRAL_MODEL = "mistral"
 
 
 class ChatModel(str, Enum):
-    
+    LLAMA_3_405B = "llama-3-405b"
     LLAMA_3_70B = "llama-3-70b"
     GPT_4o = "gpt-4o"
     GPT_3_5_TURBO = "gpt-3.5-turbo"
@@ -23,6 +24,7 @@ class ChatModel(str, Enum):
 
 
 model_mappings: dict[ChatModel, str] = {
+    ChatModel.LLAMA_3_405B: LLAMA_3_405B,
     ChatModel.GPT_3_5_TURBO: GPT3_MODEL,
     ChatModel.GPT_4o: GPT4_MODEL,
     ChatModel.LLAMA_3_70B: LLAMA_70B_MODEL,
