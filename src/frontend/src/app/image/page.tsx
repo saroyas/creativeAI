5be7c1e6f7dd41c7be476c5412df872e9191ba92
Shehryar Saroya <shehryar.saroya@gmail.com>
2024-07-24 19:00:23 +0100

    navbar

--- src/frontend/src/app/image/page.tsx
+++ src/frontend/src/app/image/page.tsx
@@ -1,11 +1,15 @@
 import { ImagePanel } from "@/components/image-panel";
+import { Navbar } from "@/components/nav";
 
 export default function Home() {
   return (
+    <div>
+    <Navbar />
     <div className="h-[100dvh] flex flex-col">
       <main className="flex-grow flex items-stretch justify-center">
         <ImagePanel />
       </main>
+    </div>
     </div>
   );
 }
